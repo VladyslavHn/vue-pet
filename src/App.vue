522c319d2fdabdd4393bf44d1ba715e69819ab0e
VladyslavHn <vlad.gnatok@gmail.com>
2024-09-09 20:53:25 +0200

    News

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,33 @@
 <template>
-<h1>asasa</h1>
+  <div class="container pt-1">
+    <div class="card">
+      <h2>News {{ now }}</h2>
+    </div>
+
+    <app-news 
+    v-for="(item, index) in news" 
+    :key="index"
+    :title="item.title"
+    :summary="item.summary">
+    </app-news>
+
+  </div>
 </template>
 
 <script>
+import AppNews from './components/AppNews.vue'
+import newsData from './News.json'
+
 export default {
-
-  name: 'App'
-
+  data () {
+    return {
+      now: new Date().toLocaleDateString(),
+      news: newsData.news
+    }
+  },
+  components: {
+    'app-news': AppNews
+  }
 }
 </script>
 
